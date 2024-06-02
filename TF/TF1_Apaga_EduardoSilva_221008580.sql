de602d4ff67953b5e3c900a36542739ca9167fb5
Eduard0803 <eduardo83204@gmail.com>
2024-06-02 14:14:49 -0300

    .

---
+++ TF/TF1_Apaga_EduardoSilva_221008580.sql
@@ -0,0 +1,27 @@
+-- --------     << TF_scripts_1 >>     ------------
+--
+--                    SCRIPT DE Remoção (DML)
+--
+-- Data Criacao ...........: 02/06/2024
+-- Autor(es) ..............: BRUNO OLIVEIRA, EDUARDO SILVA
+-- Banco de Dados .........: MySQL
+-- Base de Dados (nome) ...: TF_scripts_1
+--
+--
+-- PROJETO => 01 Base de Dados
+--         => 11 Tabelas
+-- -----------------------------------
+
+USE TF_scripts_1;
+
+DROP TABLE telefone;
+DROP TABLE ITEM_PEDIDO;
+DROP TABLE PEDIDO;
+DROP TABLE PAGAMENTO_VENDA;
+DROP TABLE ITEM_VENDA;
+DROP TABLE VENDA;
+DROP TABLE PRODUTO;
+DROP TABLE MARCA;
+DROP TABLE CATEGORIA;
+DROP TABLE CLIENTE;
+DROP TABLE METODOS_DE_PAGAMENTO;
